f08f77ce6c8a493ba83506a15cb423c459ffe17a
HCX0819 <cheexun9712@gmail.com>
2025-05-01 17:55:20 +0800

    addNewVehicle

--- mainMenu/ViewAndRentVehicles_Function.java
+++ mainMenu/ViewAndRentVehicles_Function.java
@@ -114,16 +114,33 @@ public class ViewAndRentVehicles_Function {
     }
 
     private static void loadSampleVehicles() {
-        // Only load vehicles if they haven't been loaded yet
         if (!vehiclesLoaded) {
+            
             VehicleRentalSystem.vehicles.add(new Car("C001", "Toyota", "Vios", 150.0, "PJD2353", "Red", 2006, "Sedan"));
             VehicleRentalSystem.vehicles.add(new Car("C002", "Honda", "City", 180.0, "JHG7766", "Black", 2021, "Sedan"));
-            VehicleRentalSystem.vehicles.add(new Car("C003", "Perodua", "Myvi", 120.0, "ABC1234", "Blue", 2018, "Hatchback" ));
-
-            VehicleRentalSystem.vehicles.add(new Motorcycle("M001", "Yamaha", "YZF-R3", 100.0, "KLM1234", "White", 2019, true));
-            VehicleRentalSystem.vehicles.add(new Motorcycle("M002", "Kawasaki", "Ninja 250", 120.0, "ABC5678", "Green", 2022, false));
+            VehicleRentalSystem.vehicles.add(new Car("C003", "Perodua", "Myvi", 120.0, "ABC1234", "Blue", 2018, "Hatchback"));
+            VehicleRentalSystem.vehicles.add(new Car("C004", "Proton", "Saga", 110.0, "WXY2345", "Silver", 2015, "Sedan"));
+            VehicleRentalSystem.vehicles.add(new Car("C005", "BMW", "320i", 300.0, "BMW1234", "White", 2020, "Sedan"));
+            VehicleRentalSystem.vehicles.add(new Car("C006", "Mercedes", "C200", 350.0, "MRC4567", "Black", 2019, "Sedan"));
+            VehicleRentalSystem.vehicles.add(new Car("C007", "Mazda", "CX-5", 250.0, "MAZ8899", "Grey", 2022, "SUV"));
+            VehicleRentalSystem.vehicles.add(new Car("C008", "Kia", "Cerato", 200.0, "KIA1122", "Red", 2018, "Sedan"));
+            VehicleRentalSystem.vehicles.add(new Car("C009", "Ford", "Focus", 190.0, "FRD3344", "Blue", 2017, "Hatchback"));
+            VehicleRentalSystem.vehicles.add(new Car("C010", "Hyundai", "Elantra", 210.0, "HYU5566", "White", 2021, "Sedan"));
+    
             
+            VehicleRentalSystem.vehicles.add(new Motorcycle("M001", "Yamaha", "YZF-R3", 100.0, "KLM1234", "White", 2019, true));
+            VehicleRentalSystem.vehicles.add(new Motorcycle("M002", "Kawasaki", "Ninja 250", 120.0, "V1P5678", "Green", 2022, false));
+            VehicleRentalSystem.vehicles.add(new Motorcycle("M003", "Honda", "CBR500R", 130.0, "HON6789", "Red", 2020, true));
+            VehicleRentalSystem.vehicles.add(new Motorcycle("M004", "Suzuki", "GSX250R", 110.0, "JHF3456", "Blue", 2021, false));
+            VehicleRentalSystem.vehicles.add(new Motorcycle("M005", "Ducati", "Monster", 180.0, "DUC1111", "Black", 2018, true));
+            VehicleRentalSystem.vehicles.add(new Motorcycle("M006", "KTM", "RC 200", 140.0, "PJG2222", "Orange", 2022, true));
+            VehicleRentalSystem.vehicles.add(new Motorcycle("M007", "Benelli", "302R", 120.0, "BEN3333", "Yellow", 2019, false));
+            VehicleRentalSystem.vehicles.add(new Motorcycle("M008", "Aprilia", "RS 660", 200.0, "APR4444", "Grey", 2023, true));
+            VehicleRentalSystem.vehicles.add(new Motorcycle("M009", "BMW", "G 310 R", 160.0, "P5555", "White", 2020, false));
+            VehicleRentalSystem.vehicles.add(new Motorcycle("M010", "Kawasaki", "Ninja 750", 220.0, "HD6666", "Black", 2017, true));
+    
             vehiclesLoaded = true;
         }
     }
+    
 }
